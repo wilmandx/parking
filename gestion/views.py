7570f6bf51788a5410ab987dbbd4a9a1d8a843c9
wilmandx <wilmandx1@hotmail.com>
2014-09-07 22:54:48 -0500

    bloqueo de view con autenticacion

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def index(request):
+	usuario=request.user
+	return HttpResponse(str(usuario.groups.values_list('name',flat=True))+"Hello, world. You're at the polls index.")
